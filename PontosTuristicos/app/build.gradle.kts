04d082cd13f939c7e5e64daccef3ce87059ca85a
Rodrigo Silva <rodrigo.santos.silva@outlook.com>
2024-10-26 11:55:09 -0300

    adicionado mapas e ajustado botões de interação

--- PontosTuristicos/app/build.gradle.kts
+++ PontosTuristicos/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.devtools.ksp") version "2.0.21-1.0.25"
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
@@ -43,6 +44,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- PontosTuristicos/app/src/main/java/br/edu/utfpr/pontosturisticos/MainActivity.kt
+++ PontosTuristicos/app/src/main/java/br/edu/utfpr/pontosturisticos/MainActivity.kt
@@ -7,32 +7,34 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.room.Room
-import br.edu.utfpr.pontosturisticos.database.AppDatabase
 import br.edu.utfpr.pontosturisticos.ui.CadastroActivity
 import br.edu.utfpr.pontosturisticos.ui.ListaActivity
 import br.edu.utfpr.pontosturisticos.utils.singleton.DatabaseSingleton
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity(), LocationListener {
+class MainActivity : AppCompatActivity(), LocationListener, OnMapReadyCallback {
     private lateinit var locationManager: LocationManager
     private lateinit var btCadastrar: FloatingActionButton
-    private lateinit var btLista: Button //temp
+    private lateinit var btLista: FloatingActionButton //temp
 
-    private var currentLatitude: Double? = null
-    private var currentLongitude: Double? = null
+    private lateinit var mMap: GoogleMap
+
+    private var currentLatitude: Double = 0.0
+    private var currentLongitude: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = DatabaseSingleton.getInstance(this).getAppDatabase()
-
-        btLista = findViewById(R.id.btLista) //temp
+        btLista = findViewById(R.id.btListar) //temp
         btCadastrar = findViewById(R.id.btCadastrar)
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
 
@@ -48,32 +50,48 @@ class MainActivity : AppCompatActivity(), LocationListener {
             return
         }
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 0f, this)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         btCadastrar.setOnClickListener { addPontoTuristico() }
-        btLista.setOnClickListener { listarPontosTuristicos(db) }
+        btLista.setOnClickListener { listarPontosTuristicos() }
     }
 
     override fun onLocationChanged(location: Location) {
         currentLatitude = location.latitude
         currentLongitude = location.longitude
+
+        if (::mMap.isInitialized) {
+            val localAtual = LatLng(currentLatitude, currentLongitude)
+            mMap.clear()
+            mMap.addMarker(MarkerOptions().position(localAtual).title("Você está aqui"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localAtual, 10f))
+        }
     }
 
     private fun addPontoTuristico() {
-        if (currentLatitude != null && currentLongitude != null) {
-            val intent = Intent(this, CadastroActivity::class.java).apply {
-                putExtra("EXTRA_LATITUDE", currentLatitude)
-                putExtra("EXTRA_LONGITUDE", currentLongitude)
-            }
-            startActivity(intent)
-        } else Toast.makeText(this, "Localização ainda não disponível. Aguarde", Toast.LENGTH_SHORT)
-            .show()
+        val intent = Intent(this, CadastroActivity::class.java).apply {
+            putExtra("EXTRA_LATITUDE", currentLatitude)
+            putExtra("EXTRA_LONGITUDE", currentLongitude)
+        }
+        startActivity(intent)
     }
 
-    private fun listarPontosTuristicos(db: AppDatabase) {
+    private fun listarPontosTuristicos() {
+        val db = DatabaseSingleton.getInstance(this).getAppDatabase()
         val ids = db.pontoTuristicoDao().getAllIds()
 
         val intent = Intent(this, ListaActivity::class.java)
         intent.putIntegerArrayListExtra("IDS_LIST", ArrayList(ids))
         startActivity(intent)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val home = LatLng(currentLatitude, currentLongitude)
+        mMap.addMarker(MarkerOptions().position(home).title("Marker in Home"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 10f))
     }
 }
